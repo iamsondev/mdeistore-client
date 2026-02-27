d74f7c221a7d812ec3bb3834823669b590b1e355
iamsondev <sondipkumarsk@gmail.com>
2026-02-27 09:45:01 +0600

    fix: revalidateTag to revalidatePath

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,4 +1,4 @@
-import { revalidateTag } from "next/cache";
+import { revalidatePath } from "next/cache";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
@@ -8,6 +8,6 @@ export async function POST(request: NextRequest) {
     return NextResponse.json({ error: "Tag is required" }, { status: 400 });
   }
 
-  revalidateTag(tag);
+  revalidatePath(`/${tag}`);
   return NextResponse.json({ revalidated: true, tag });
 }
