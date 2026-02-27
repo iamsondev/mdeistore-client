565e1f9629adff91e3535ab4fd369e3d210c882c
iamsondev <sondipkumarsk@gmail.com>
2026-02-28 01:10:49 +0600

    again change the backend url

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,5 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: "https://medistore-server-fawn.vercel.app",
+  baseURL: "https://medistore-client-bice.vercel.app",
 });
